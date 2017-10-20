8d261f9233f6af0b2412864c9536767ec473f417
Kidd Wang <kiddkid99@gmail.com>
2017-10-20 19:55:37 +0800

    Add algorithm fibonacci and tests

---
+++ Algorithm_Practices.UnitTests/FibonacciTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace Algorithm_Practices.UnitTests
+{
+    [TestFixture]
+    public class FibonacciTests
+    {
+        [TestCase(0, 0)]
+        [TestCase(1, 1)]
+        public void Fibonacii_WhenCalled_ReturnCorrect(int number, long expected)
+        {
+            int result = Algorithm.Fibonacci(number);
+
+
+            Assert.AreEqual(result, expected);
+            
+        }
+    }
+}
---
+++ Algorithm_Practices/Algorithm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_Practices
+{
+    public class Algorithm
+    {
+        public static int Fibonacci(int number)
+        {
+            return 0;
+        }
+    }
+}
